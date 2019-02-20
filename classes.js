3d7a61d54803cbd66b04d19ca3ec570b1a237a5f
Nikita Gaidakov <ngaidakov@podfather.com>
2019-02-20 09:24:10 +0000

    Extract class definitions to separate js file

---
+++ classes.js
@@ -0,0 +1,29 @@
+function Pos(x, y)
+{
+  this.x = x * scale
+  this.y = y * scale
+  this.img = null
+}
+
+Pos.prototype.isEmpty = function ()
+{
+  return this.img === null
+}
+
+Pos.prototype.populate = function (image)
+{
+  this.img = image
+  this.draw()
+}
+
+Pos.prototype.draw = function ()
+{
+  if (this.img === null) return
+
+  const xPos = this.x - (this.img.width / 2)
+  const yPos = this.y - (this.img.height / 2)
+
+  ctx.drawImage(this.img, xPos, yPos, this.img.width, this.img.height)
+  ctx.drawImage(foreground, 0, 0, foreground.width, foreground.height)
+}
+

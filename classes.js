1871f0513f44312148077471ee5a02c3ac139ad0
Nikita Gaidakov <ngaidakov@podfather.com>
2019-02-20 16:56:19 +0000

    Assign prototype in one go

--- classes.js
+++ classes.js
@@ -5,26 +5,28 @@ function Face(x, y)
   this.img = null
 }
 
-Face.prototype.spawn = function (i)
-{
-  const nonexistentPerson = new Image(imageSize, imageSize)
-  nonexistentPerson.src = `https://thispersondoesnotexist.com/?${i}`
-  this.img = nonexistentPerson
+Face.prototype = {
+  spawn: function (i)
+  {
+    const nonexistentPerson = new Image(imageSize, imageSize)
+    nonexistentPerson.src = `https://thispersondoesnotexist.com/?${i}`
+    this.img = nonexistentPerson
 
-  nonexistentPerson.addEventListener('load', () => {
-    console.log(`nonexistent person ${i} loaded`)
-    this.draw()
-  })
+    nonexistentPerson.addEventListener('load', () =>
+    {
+      console.log(`nonexistent person ${i} loaded`)
+      this.draw()
+    })
+  },
+
+  draw: function ()
+  {
+    if (this.img === null) return
+
+    const xPos = this.x - (this.img.width / 2)
+    const yPos = this.y - (this.img.height / 2)
+
+    ctx.drawImage(this.img, xPos, yPos, this.img.width, this.img.height)
+    ctx.drawImage(foreground, 0, 0, foreground.width, foreground.height)
+  }
 }
-
-Face.prototype.draw = function ()
-{
-  if (this.img === null) return
-
-  const xPos = this.x - (this.img.width / 2)
-  const yPos = this.y - (this.img.height / 2)
-
-  ctx.drawImage(this.img, xPos, yPos, this.img.width, this.img.height)
-  ctx.drawImage(foreground, 0, 0, foreground.width, foreground.height)
-}
-
